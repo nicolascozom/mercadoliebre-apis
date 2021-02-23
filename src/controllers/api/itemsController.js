88c114973706bdc4e6cb12169d3911829b1a64c3
nicolascozom <nicolascozom@gmail.com>
2021-02-22 21:19:56 -0300

    nodemon y esquema de endpoint

--- src/controllers/api/itemsController.js
+++ src/controllers/api/itemsController.js
@@ -7,11 +7,25 @@ module.exports = {
 	index (req, res){
 
         let respuesta = {
-            meta: {
-                status: 200,
-            },
-            data: "hola, soy la data",
-          }
+                "meta": {
+                    "status": 201,
+                    "message": "Product added to cart"
+                },
+                "data": {
+                    "item": {
+                        "id": 126,
+                        "salePrice": 4968,
+                        "quantity": "1",
+                        "subTotal": 4968,
+                        "state": 1,
+                        "userId": 102,
+                        "sellerId": 101,
+                        "productId": 221,
+                        "updatedAt": "2020-10-01T19:36:24.590Z",
+                        "createdAt": "2020-10-01T19:36:24.590Z"
+                    }
+                }
+        }
 
 		res.json(respuesta)
     }
